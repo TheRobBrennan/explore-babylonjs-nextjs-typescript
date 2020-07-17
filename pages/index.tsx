d51d85f6d00b8be6b7f3f87e99a8676e1948c098
Rob Brennan <rob@therobbrennan.com>
2020-07-16 18:03:47 -0700

    Removed unnecessary import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next"
 import Example00Container from "../src/containers/Example-00-Container"
-import SceneComponent from "../src/components/SceneComponent"
 
 const IndexPage: NextPage = () => {
   return (
