6ec6c1d48208fce9103c946378a6a42353d5fe1c
Rob Brennan <rob@therobbrennan.com>
2020-07-16 19:55:21 -0700

    Successfully able to load and interact with an OBJ asset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from "next"
-import Example00Container from "../src/containers/Example-00-Container"
+import ExampleContainer from "../src/containers/Example-01-Container"
 
 const IndexPage: NextPage = () => {
   return (
     <>
-      <Example00Container />
+      <ExampleContainer />
     </>
   )
 }
